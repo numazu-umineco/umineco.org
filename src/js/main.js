59d790ac441bac9aeb7b145307149eccef127e6a
windyakin <windyakin@pepabo.com>
2023-07-04 21:41:27 +0900

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 // import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import axios from 'axios';
+
+window.addEventListener('load', async () => {
+  try {
+    const response = await axios.get('https://discord.com/api/v9/invites/8taubbJ5qD?with_counts=true&with_expiration=true');
+    console.log(response.data);
+    const memberCount = response.data.approximate_member_count;
+    document.querySelectorAll('.member-count').forEach((element) => {
+      element.textContent = memberCount;
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
