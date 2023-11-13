3efcb98e1215bf1fa23aa7e6a0cfafb724806bab
windyakin <windyakin@gmail.com>
2023-11-13 12:49:46 +0900

    test

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 const htmlmin = require('html-minifier');
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const markdownItEleventyImg = require("markdown-it-eleventy-img");
+const path = require("path");
+
 function eleventyConfig(config) {
   config.addPassthroughCopy("src/img");
   config.addPassthroughCopy("src/css");
@@ -19,6 +24,34 @@ function eleventyConfig(config) {
   }
 
   config.addTransform("htmlmin", htmlMinify);
+
+  config.addFilter("tags", (tags) => {
+    return (tags || []).filter(tag => ["all", "articles"].indexOf(tag) === -1);
+  });
+
+  // Customize Markdown library and settings:
+  let markdownLibrary = markdownIt({
+    html: true,
+    linkify: true
+  }).use(markdownItAnchor, {
+    permalink: markdownItAnchor.permalink.ariaHidden({
+      placement: "before",
+      class: "anchor-link",
+      symbol: "#"
+    }),
+    level: [1, 2, 3, 4],
+  }).use(markdownItEleventyImg, {
+    imgOptions: {
+      outputDir: "_site/img/optimized/",
+      urlPath: "/img/optimized/"
+    },
+    resolvePath: (filepath, env) => path.join(path.dirname(env.page.inputPath), filepath),
+    globalAttributes: {
+      class: "img-fluid img-thumbnail",
+      decoding: "async"
+    },
+  });
+  config.setLibrary("md", markdownLibrary);
 
   return {
     dir: {
---
+++ src/news/20231111/jimoai_festival_2023_flower_stand.md
@@ -0,0 +1,11 @@
+---
+title: 『沼津地元愛まつり 2023』にてフラワースタンド企画を実施しました
+eyecatch: flower_stand.jpg
+---
+
+2023年10月「ラブライブ！サンシャイン!! 沼津地元愛まつり 2023
+
+![](flower_stand.jpg)
+
+
+協力: MIDORIYA 様
